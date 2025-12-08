ba124902ff3c35b014b38f1d734727eba868a482
25B11CS793 <narayanapunnam086@gmail.com>
2025-12-08 21:39:59 +0530

    Add files via upload

---
+++ sum of 4 and 8. code chef.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 4;   // first number
+    int b = 8;   // second number
+    int sum = a + b;  // compute sum
+
+    printf("%d\n", sum);  // print result
+    return 0;
+}
